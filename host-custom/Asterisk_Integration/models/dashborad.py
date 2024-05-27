734c1909483578da91a84f43760e56b7c5fc4bc8
Ala Eddine Khiari <135550744+AlaEddine-Khiari@users.noreply.github.com>
2024-05-27 20:28:33 +0100

    Delete host-custom directory

--- host-custom/Asterisk_Integration/models/dashborad.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class CDRLog(models.Model):
-    _inherit = 'cdr.log'
-
-    #Field to extract the Outgoing and Incoming Calls
-    length = fields.Integer(string='Length', store=True)
--- host-custom/base_phone/models/ir_fields_converter.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, models
-
-
-class IrFieldsConverter(models.AbstractModel):
-    _inherit = "ir.fields.converter"
-
-    @api.model
-    def _str_to_phone(self, model, field, value):
-        return super()._str_to_char(model, field, value)
